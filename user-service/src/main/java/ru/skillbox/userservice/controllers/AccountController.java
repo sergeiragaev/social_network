7b1d110fa059c7e46197f025494c7848b30a6360
Sergey Lunev <lunev.s@gmail.com>
2024-06-14 16:20:46 +0300

    - added controllers - added base module configuration - added errorhandler

---
+++ user-service/src/main/java/ru/skillbox/userservice/controllers/AccountController.java
@@ -0,0 +1,92 @@
+package ru.skillbox.userservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skillbox.userservice.data.dto.AccountByFilterDto;
+import ru.skillbox.userservice.data.dto.AccountDto;
+import ru.skillbox.userservice.data.dto.AccountRecoveryRq;
+import ru.skillbox.userservice.data.dto.AccountSearchDto;
+import ru.skillbox.userservice.data.dto.Pageable;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+    @PutMapping("/recovery")
+    public ResponseEntity<String> recoveryUserAccount(@RequestBody AccountRecoveryRq recoveryRq) {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<AccountDto> getUserAccount() {
+        return ResponseEntity.ok(new AccountDto());
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<AccountDto> updateUserAccount(@RequestBody AccountDto accountDto) {
+        return ResponseEntity.ok(new AccountDto());
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<String> deleteUserAccount() {
+        return ResponseEntity.ok("");
+    }
+
+    @PutMapping("/block/{id}")
+    public ResponseEntity<String> blockAccountById(@PathVariable Integer id) {
+        return ResponseEntity.ok("");
+    }
+
+    @DeleteMapping("/block/{id}")
+    public ResponseEntity<String> unblockAccountById(@PathVariable Integer id) {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllAccounts(@RequestParam Pageable page) {
+        return ResponseEntity.ok(List.of());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createAccount(@RequestBody AccountDto accountDto) {
+        return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/searchByFilter")
+    public ResponseEntity<AccountDto> searchAccountByFilter(@RequestBody AccountByFilterDto filterDto) {
+        return ResponseEntity.ok(new AccountDto());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Integer id) {
+        return ResponseEntity.ok(new AccountDto());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<AccountDto> searchAccount(@RequestParam AccountSearchDto searchDto, @RequestParam Pageable page) {
+        return ResponseEntity.ok(new AccountDto());
+    }
+
+    @GetMapping("/ids")
+    public ResponseEntity<?> getAllIds() {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/accountIds")
+    public ResponseEntity<?> getAccountIds(@RequestParam Integer[] ids, @RequestParam Pageable page) {
+        return ResponseEntity.ok(new AccountDto());
+    }
+}
