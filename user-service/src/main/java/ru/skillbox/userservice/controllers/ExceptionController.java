edc96cb9a4a52950bb0bbd17e93798acad567d50
Sergey Lunev <lunev.s@gmail.com>
2024-06-14 16:20:46 +0300

    - added controllers - added base module configuration - added errorhandler

---
+++ user-service/src/main/java/ru/skillbox/userservice/controllers/ExceptionController.java
@@ -0,0 +1,51 @@
+package ru.skillbox.userservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.skillbox.userservice.data.model.ErrorDetail;
+
+import java.nio.file.AccessDeniedException;
+import java.util.Date;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler({ChangeSetPersister.NotFoundException.class, ObjectNotFoundException.class, HttpClientErrorException.NotFound.class})
+    public ResponseEntity<?> notFoundObject(Exception exception) {
+        return templateResponseException(exception, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> accessDenied(AccessDeniedException exception) {
+        return templateResponseException(exception, HttpStatus.FORBIDDEN);
+    }
+
+
+    private ResponseEntity<?> templateResponseException(String message, Exception exception, HttpStatus status) {
+        return ResponseEntity.status(status.value())
+                .body(createDefaultErrorDetail(message, exception, status.value()));
+    }
+
+    private ResponseEntity<?> templateResponseException(Exception exception, HttpStatus status) {
+        return templateResponseException(exception.getMessage(), exception, status);
+    }
+
+    private ErrorDetail createDefaultErrorDetail(String message, Exception exception, int statusValue) {
+        return new ErrorDetail()
+                .setTitle(message)
+                .setStatus(statusValue)
+                .setTimeStamp(new Date().getTime())
+                .setDetail(exception.getMessage())
+                .setDeveloperMessage(exception.getClass().getName());
+    }
+
+}
---
+++ user-service/src/main/java/ru/skillbox/userservice/controllers/StorageController.java
@@ -0,0 +1,21 @@
+package ru.skillbox.userservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skillbox.userservice.data.dto.AccountDto;
+
+@RestController
+@RequestMapping("/storage")
+@RequiredArgsConstructor
+public class StorageController {
+
+    @PostMapping
+    public ResponseEntity<AccountDto> uploadFile(@RequestBody MultipartFile file) {
+        return ResponseEntity.ok(new AccountDto());
+    }
+}
