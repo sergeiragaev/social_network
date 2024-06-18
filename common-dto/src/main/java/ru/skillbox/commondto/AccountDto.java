316cd37201436eeea07cc354c880670312856bd6
kerby <kerby@inbox.ru>
2024-06-18 22:49:17 +0500

    added module 'common-dto'

---
+++ common-dto/src/main/java/ru/skillbox/commondto/AccountDto.java
@@ -0,0 +1,35 @@
+package ru.skillbox.commondto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AccountDto {
+    long id;
+    String email;
+    String phone;
+    String photo;
+    String about;
+    String city;
+    String country;
+    String token;
+    StatusCode statusCode;
+    String firstName;
+    String lastName;
+    LocalDateTime regDate;
+    LocalDateTime birthDate;
+    String messagePermission;
+    LocalDateTime lastOnlineTime;
+    boolean isOnline;
+    boolean isBlocked;
+    boolean isDeleted;
+    String photoId;
+    String photoName;
+    Role role;
+    LocalDateTime createdOn;
+    LocalDateTime updatedOn;
+    String password;
+}
+
+
