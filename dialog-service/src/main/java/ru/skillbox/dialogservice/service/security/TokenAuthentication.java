91b6fcebca475ee4e9121e4dbbecbe830b9882ac
Sergei Ragaev <sergei.ragaev@gmail.com>
2024-07-24 08:21:44 +0300

    - added authorities into token claims for creating  principal for adding to map of web socket sessions

---
+++ dialog-service/src/main/java/ru/skillbox/dialogservice/service/security/TokenAuthentication.java
@@ -0,0 +1,52 @@
+package ru.skillbox.dialogservice.service.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TokenAuthentication implements Authentication {
+
+    private final List<GrantedAuthority> authorities;
+    private final boolean authenticated;
+    private final String account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Principal getPrincipal() {
+        return () -> account;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return account;
+    }
+}
