f33d8c6b1bf84fe76ab0d8be9e7ab7de8f67fd33
Сергей Рагаев <ragaev@mail.ru>
2024-09-24 15:32:42 +0300

    @RequestHeader("id") deleted from controllers methods

--- audit-service/src/test/java/ru/skillbox/auditservice/AuditServiceApplicationTests.java
+++ audit-service/src/test/java/ru/skillbox/auditservice/AuditServiceApplicationTests.java
@@ -2,8 +2,10 @@ package ru.skillbox.auditservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class AuditServiceApplicationTests {
 
 	@Test
